7bcd99aa8a49cc5fe8f377094270b02771e2b5a9
Paola L <paolalarrazabal@gmail.com>
2019-11-05 14:13:06 -0500

    added moment to calculate arrival time and minutes till next train, added firebase .ref to save inputs into firebase

--- javascript/train.js
+++ javascript/train.js
@@ -11,7 +11,65 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-let name = $("#name-input").val.trim; 
-let destination = $("#destination-input").val.trim;
-let firstTrainTime = $("#time-input").val.trim;
-let frequency = $("#frequency-input").val.trim;
+let database = firebase.database(); 
+
+let name = "";
+let destination = ""; 
+let firstTrainTime = "";
+let frequency = "";
+let minutesAway = "";
+let arrivalTime = ""; 
+
+
+$("#add-train").on("click", function(event) {
+event.preventDefault(); 
+
+name = $("#name-input").val().trim(); 
+destination = $("#destination-input").val().trim();
+firstTrainTime = $("#time-input").val().trim();
+frequency = $("#frequency-input").val().trim();
+
+let firstTrainTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+console.log(firstTrainTimeConverted);
+
+let currentTime = moment(); 
+console.log("current time:" + moment(currentTime).format("hh:mm"));
+
+let difference = moment().diff(moment(firstTrainTimeConverted), "minutes");
+console.log("difference in time: " + difference);
+
+let remainder = difference % frequency;
+console.log(remainder);
+
+let minutesAway = frequency - remainder;
+console.log("minutes away: "+ minutesAway);
+
+let nextArrival = moment().add(minutesAway, "minutes");
+let arrivalTime =(moment(nextArrival).format("hh;mm"));
+console.log("arrival time: " + arrivalTime);
+
+
+
+database.ref().push( {
+  name: name, 
+  destination: destination,
+  firstTrainTime: firstTrainTime, 
+  minutesAway: minutesAway,
+  arrivalTime: arrivalTime
+
+});
+
+console.log(name);
+console.log(destination);
+console.log(frequency); 
+console.log(firstTrainTime);
+
+});
+
+function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+    };
+
+
+
+
